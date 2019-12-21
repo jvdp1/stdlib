c2edbafd2f81284476ee99a5a49f87cf006ae9cf
Vandenplas, Jeremie <jeremie.vandenplas@gmail.com>
2019-12-21 16:59:10 +0100

    stdlib_experimental_io: modification of sp case + addition of qp
    *Modification of the sp case to avoid additional temporary arrays (+ to make it more general)
    *Addition of qp case
    *Proposition of using unlimited polymorphic objects

--- src/tests/loadtxt/test_savetxt.f90
+++ src/tests/loadtxt/test_savetxt.f90
@@ -1,11 +1,13 @@
 program test_loadtxt
-use iso_fortran_env, only: sp=>real32, dp=>real64
-use stdlib_experimental_io, only: loadtxt, savetxt
+use iso_fortran_env, only: sp=>real32, dp=>real64 ,qp=>real128
+use stdlib_experimental_io, only: loadtxt, savetxt ,savetxt_poly
 use stdlib_experimental_error, only: assert
 implicit none
 
 call test_sp()
 call test_dp()
+call test_dp_poly()
+call test_qp()
 
 contains
 
@@ -42,4 +44,35 @@ contains
     call assert(all(abs(e-d2) < epsilon(1._dp)))
     end subroutine
 
+    subroutine test_qp()
+    real(qp) :: d(3, 2), e(2, 3)
+    real(qp), allocatable :: d2(:, :)
+    d = reshape([1, 2, 3, 4, 5, 6], [3, 2])
+    call savetxt("tmp.dat", d)
+    call loadtxt("tmp.dat", d2)
+    call assert(all(shape(d2) == [3, 2]))
+    call assert(all(abs(d-d2) < epsilon(1._qp)))
+
+    e = reshape([1, 2, 3, 4, 5, 6], [2, 3])
+    call savetxt("tmp.dat", e)
+    call loadtxt("tmp.dat", d2)
+    call assert(all(shape(d2) == [2, 3]))
+    call assert(all(abs(e-d2) < epsilon(1._qp)))
+    end subroutine
+
+    subroutine test_dp_poly()
+    real(dp) :: d(3, 2), e(2, 3)
+    real(dp), allocatable :: d2(:, :)
+    d = reshape([1, 2, 3, 4, 5, 6], [3, 2])
+    call savetxt_poly("tmp.dat", d)
+    call loadtxt("tmp.dat", d2)
+    call assert(all(shape(d2) == [3, 2]))
+    call assert(all(abs(d-d2) < epsilon(1._dp)))
+
+    e = reshape([1, 2, 3, 4, 5, 6], [2, 3])
+    call savetxt_poly("tmp.dat", e)
+    call loadtxt("tmp.dat", d2)
+    call assert(all(shape(d2) == [2, 3]))
+    call assert(all(abs(e-d2) < epsilon(1._dp)))
+    end subroutine
 end program
