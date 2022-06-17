253a0c80e0708a36353d8950b07f5311e0286ae3
jvdp1 <jeremie.vandenplas@gmail.com>
2022-06-17 21:33:12 +0000

    Deploying to stdlib-fpm from @ jvdp1/stdlib@c4d4ec77c1ece910ca43878b803ad65e6e953b72 🚀

--- src/stdlib_version.f90
+++ src/stdlib_version.f90
@@ -11,16 +11,16 @@ module stdlib_version
 
 
     !> String representation of the standard library version
-    character(len=*), parameter :: stdlib_version_string = "0.1.0"
+    character(len=*), parameter :: stdlib_version_string = "0.2.1"
 
     !> Major version number of the above standard library version
     integer, parameter :: stdlib_major = 0
 
     !> Minor version number of the above standard library version
-    integer, parameter :: stdlib_minor = 1
+    integer, parameter :: stdlib_minor = 2
 
     !> Patch version number of the above standard library version
-    integer, parameter :: stdlib_patch = 0
+    integer, parameter :: stdlib_patch = 1
 
     !> Compact numeric representation of the standard library version
     integer, parameter :: stdlib_version_compact = &
