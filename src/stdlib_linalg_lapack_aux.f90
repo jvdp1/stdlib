9e839a2637ce40d4ac63fb4bcc383e7c1d177e5c
jvdp1 <jeremie.vandenplas@gmail.com>
2026-01-26 09:48:30 +0000

    Deploying to stdlib-fpm from @ jvdp1/stdlib@4793a64748c5db824227a8eb374205ba847e5c3b 🚀

--- src/stdlib_linalg_lapack_aux.f90
+++ src/stdlib_linalg_lapack_aux.f90
@@ -148,6 +148,8 @@ module stdlib_linalg_lapack_aux
         ! =====================================================================
            ! Executable Statements
            stdlib_ieeeck = 1
+
+#if __INTEL_COMPILER_BUILD_DATE >= 20230101
            
            ! Test support for infinity values
            if (.not.ieee_support_inf(one)) then 
@@ -162,7 +164,8 @@ module stdlib_linalg_lapack_aux
               stdlib_ieeeck = 0
               return
            end if
-           
+#endif
+
            return
      end function stdlib_ieeeck
 
